f942ca5ab6c677490dcf0130d4967dba610fa6bb
prathamesh.a <prathamesh.a@internal.media.net>
2024-08-12 21:17:07 +0530

    added functionality of change id on drag and drop

---
+++ script.js
@@ -0,0 +1,46 @@
+let imageElement = document.querySelectorAll('.image');
+
+// console.log(imageElement);
+
+imageElement.forEach(element => {
+    element.addEventListener('dragstart', (e) => {
+        // console.log('drag start', e.target.id)
+        element.style.opacity = 0.5;
+        
+        e.dataTransfer.setData("text", e.target.id)
+    })
+
+    element.addEventListener('dragend', (e) => {
+        // console.log('drag end', e.target.id)
+        element.style.opacity = 1;  
+
+    })
+
+    let eventsArray = ["dragover", "dragenter", "drop"];
+
+    eventsArray.forEach(dragEvent => {
+        // console.log(dragEvent)
+
+        let dropElement = document.querySelectorAll('.image');
+
+        dropElement.forEach(element => {
+            element.addEventListener(dragEvent, (e) => {
+                e.preventDefault();
+
+                if(dragEvent == "drop"){
+
+                    var draggedElementId = e.dataTransfer.getData("text");
+
+                    var draggedElement = document.getElementById(draggedElementId);
+                    var dropTarget = e.target;
+
+                    var tempId = draggedElement.id;
+                    draggedElement.id = dropTarget.id;
+                    dropTarget.id = tempId;
+                }
+            })
+        })
+        
+    })
+
+})
